66d7c494d0a192cf010cd165b99911892472d95d
rpmhasan <rpm.hasan@gmail.com>
2015-05-13 15:40:11 +0600

    Initital Upload

---
+++ EmployeeInfoApp/EmployeeInfoUI.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace EmployeeInfoApp
+{
+    partial class EmployeeInfoUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.salaryTextBox = new System.Windows.Forms.TextBox();
+            this.showButton = new System.Windows.Forms.Button();
+            this.showDataGridView = new System.Windows.Forms.DataGridView();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.deleteIDTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.showDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(233, 184);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 0;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(2, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Address:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(18, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Email:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(14, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Salary:";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameTextBox.Location = new System.Drawing.Point(69, 32);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(238, 22);
+            this.nameTextBox.TabIndex = 5;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailTextBox.Location = new System.Drawing.Point(70, 150);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(237, 22);
+            this.emailTextBox.TabIndex = 6;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addressTextBox.Location = new System.Drawing.Point(70, 58);
+            this.addressTextBox.Multiline = true;
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(237, 86);
+            this.addressTextBox.TabIndex = 7;
+            // 
+            // salaryTextBox
+            // 
+            this.salaryTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.salaryTextBox.Location = new System.Drawing.Point(70, 185);
+            this.salaryTextBox.Name = "salaryTextBox";
+            this.salaryTextBox.Size = new System.Drawing.Size(145, 22);
+            this.salaryTextBox.TabIndex = 8;
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(187, 240);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(120, 23);
+            this.showButton.TabIndex = 9;
+            this.showButton.Text = "Show All";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // showDataGridView
+            // 
+            this.showDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.showDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.showDataGridView.GridColor = System.Drawing.SystemColors.ControlLight;
+            this.showDataGridView.Location = new System.Drawing.Point(341, 31);
+            this.showDataGridView.Name = "showDataGridView";
+            this.showDataGridView.Size = new System.Drawing.Size(583, 424);
+            this.showDataGridView.TabIndex = 10;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(232, 378);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 11;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "Name";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "Address";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column3.HeaderText = "Email";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column4.HeaderText = "Salary";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // deleteIDTextBox
+            // 
+            this.deleteIDTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteIDTextBox.Location = new System.Drawing.Point(81, 379);
+            this.deleteIDTextBox.Name = "deleteIDTextBox";
+            this.deleteIDTextBox.Size = new System.Drawing.Size(145, 22);
+            this.deleteIDTextBox.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(20, 381);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Enter ID:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(2, 350);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(127, 16);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "To Delete Any Row:";
+            // 
+            // EmployeeInfoUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 467);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.deleteIDTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.showDataGridView);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.salaryTextBox);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.saveButton);
+            this.Name = "EmployeeInfoUI";
+            this.Text = "Employee Information";
+            ((System.ComponentModel.ISupportInitialize)(this.showDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.TextBox salaryTextBox;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.DataGridView showDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.TextBox deleteIDTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ EmployeeInfoApp/EmployeeInfoUI.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeeInfoApp
+{
+    public partial class EmployeeInfoUI : Form
+    {
+        public EmployeeInfoUI()
+        {
+            InitializeComponent();
+        }
+        string connectionstring = @"SERVER=PC-301-25\SQLEXPRESS;Database=EmployeeIntroDB;integrated security=true";
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            string name = nameTextBox.Text;
+            string address = addressTextBox.Text;
+            string email = emailTextBox.Text;
+            int salary = Convert.ToInt32(salaryTextBox.Text);
+
+            if (name != "" || address != "" || email != "")
+            {
+                if (salary>0)
+                {
+                    SqlConnection aSqlConnection = new SqlConnection(connectionstring);
+                    string query = "INSERT INTO EmployeeInfoTable VALUES ('" + name + "','" + address + "','" + email + "','" + salary + "')";
+                    SqlCommand aSqlCommand = new SqlCommand(query, aSqlConnection);
+                    aSqlConnection.Open();
+                    int rowAffected = aSqlCommand.ExecuteNonQuery();
+                    if (rowAffected > 0)
+                    {
+                        MessageBox.Show("Done");
+                    }
+                    aSqlConnection.Close();
+
+                    nameTextBox.Clear();
+                    addressTextBox.Clear();
+                    emailTextBox.Clear();
+                    salaryTextBox.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Enter valid salary!");
+                }
+                
+                
+            }
+            else
+            {
+                MessageBox.Show("Please Enter All items!!!");
+            }
+
+        }
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            SqlConnection aSqlConnection = new SqlConnection(connectionstring);
+            aSqlConnection.Open();
+            string query = "SELECT * FROM EmployeeInfoTable";
+
+            SqlCommand aSqlCommand = new SqlCommand(query, aSqlConnection);
+            SqlDataReader reader = aSqlCommand.ExecuteReader();
+            showDataGridView.Rows.Clear();
+            while(reader.Read())
+            {
+                showDataGridView.Rows.Add(reader[0].ToString(), reader[1].ToString(), reader[2].ToString(), reader[3].ToString(),
+                    reader[4].ToString());
+            }
+            aSqlConnection.Close();
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            int deleteID = Convert.ToInt32(deleteIDTextBox.Text);
+            SqlConnection aSqlConnection = new SqlConnection(connectionstring);
+            aSqlConnection.Open();
+
+            string query = "DELETE FROM EmployeeInfoTable WHERE id = '"+deleteID+"'";
+            SqlCommand aSqlCommand = new SqlCommand(query, aSqlConnection);
+            int rowAffected = aSqlCommand.ExecuteNonQuery();
+            if (rowAffected > 0)
+            {
+                MessageBox.Show("Deleted!");
+            }
+            aSqlConnection.Close();
+            deleteIDTextBox.Clear();
+
+        }
+    }
+}
